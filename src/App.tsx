76310359bfd66e361d693e46efac4f1c7d8db298
김연호 <kimfine000@gmail.com>
2026-05-09 17:59:59 +0900

    11. (2.3) Tailwind CSS 핵심정리 2

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,21 @@ function App() {
       <div className="h-20 w-full bg-blue-500">box</div>
 
       {/* 여백 */}
-      <div className="h-50 w-50 bg-red-400 pt-5 pr-5 pl-5 m-5">
+      <div className="m-5 h-50 w-50 bg-red-400 pt-5 pr-5 pl-5">
         <div className="h-full w-full bg-blue-400"></div>
+      </div>
+
+      {/* 보더 */}
+      <div className="m-5 h-50 w-50 rounded-md border-t border-r border-b border-l bg-yellow-500">
+        border
+      </div>
+
+      { /* 플렉스 컨테이너 */ }
+      <div className="flex flex-row justify-evenly items-center">
+        <div className="h-10 w-10 border">1</div>
+        <div className="h-20 w-10 flex-1 border">2</div>
+        <div className="h-30 w-10 border">3</div>
+        <div className="h-40 w-10 border">4</div>
       </div>
     </div>
   );
